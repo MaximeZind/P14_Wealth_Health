70043990c7d4e3b0831d66a96abfae6dbe3754b4
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-08-12 18:42:06 +0200

    rename des dossiers, installation de react router

---
+++ WealthHealth_FrontEnd/src/router/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+              
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
